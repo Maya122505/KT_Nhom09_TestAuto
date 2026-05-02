b7dee6f2c1ba178830349861460548d7c8c46d6c
DinhThuc1012 <thithuc1012@gmail.com>
2026-05-02 17:37:47 +0700

    Update GeneralPage.java

--- src/main/java/pageobjects/GeneralPage.java
+++ src/main/java/pageobjects/GeneralPage.java
@@ -1,4 +1,17 @@
 package pageobjects;
 
+import common.Constant;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 public class GeneralPage {
+    protected WebDriverWait wait = new WebDriverWait(Constant.WEBDRIVER, Duration.ofSeconds(15));
+
+    private final By _menuThucHienDeThi = By.xpath("//a[@href='/quiz/thuc-hien-de-thi/']");
+
+    public void vaoMenuThucHienDeThi() {
+        wait.until(ExpectedConditions.elementToBeClickable(_menuThucHienDeThi)).click();
+    }
 }
